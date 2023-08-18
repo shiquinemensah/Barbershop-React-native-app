bcafc6df48975f0e1306eaa6d1fba7b13598304b
Shiquine Godchild <godchildshiquine@gmail.com>
2023-08-18 10:02:37 +0100

    barber project

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, Image, StyleSheet} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+const App = () => {
+  return(
+    <View style = {styles.container}>
+      <Text style={styles.Text}>Hello world</Text>
+      <Image source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}} style={styles.img}></Image>
     </View>
   );
-}
+}; hello world 
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+
+  Text:{
+    fontSize: 30,
+    color: 'blue',
+  },
+
+  img:{
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  }
 });
+
+
+export default App;
